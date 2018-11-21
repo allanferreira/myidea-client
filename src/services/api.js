8a6a4629726a4ef986784b6f05ee8a2ec012dc7d
Allan Ferreira <allan.less@outlook.com>
2018-11-21 10:59:33 -0200

    api correta

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
-  // baseURL: 'http://142.93.158.20/api/',
+  // baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'http://142.93.158.20/api/',
   headers: {
     'Content-Type': 'application/json',
      Accept: 'application/json',
